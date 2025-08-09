6cabc054061bafcba953ffa956614ffbea016adb
Stefan Richter <stefan@chkk.io>
2025-08-09 13:56:55 -0700

    Release v0.0.5
    Closes #5.
    
    Signed-off-by: Stefan Richter <44383+02strich@users.noreply.github.com>

--- Formula/c/chkk.rb
+++ Formula/c/chkk.rb
@@ -1,7 +1,7 @@
 class Chkk < Formula
   desc "Your Upgrade Copilot for k8s, Add-ons, Application services & 100s of Projects"
   homepage "https://chkk.io"
-  version "0.0.4"
+  version "0.0.5"
 
   bottle do
     root_url "https://ghcr.io/v2/chkk-io/cli"
@@ -13,25 +13,25 @@ class Chkk < Formula
   on_macos do
     if Hardware::CPU.arm?
       url "https://cli.chkk.io/download/v#{version}/chkk-darwin-arm64"
-      sha256 "cea9825be3525c04fb6075f72275f9fbb502b9f76714706e2727ec9eb16459c9"
+      sha256 "2cd0ca387d752dc399bb623a74dd10b4736b25aa615bf167fb6799ab96576465"
 
     end
 
     if Hardware::CPU.intel?
       url "https://cli.chkk.io/download/v#{version}/chkk-darwin-amd64"
-      sha256 "4de32cb8c7a8bf1de4fee20f7e423922c4dbc3fd87f7ab4bb5cc210a1c5b5486"
+      sha256 "875d5951a4c29863d53ee4826b74f53d6e463f51b2c5d9ff84d8999606245c08"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
       url "https://cli.chkk.io/download/v#{version}/chkk-linux-amd64"
-      sha256 "acbaed1c01510bd8987aa6a450290d29291fcea3aa6b521bfa8abab2c143fe13"
+      sha256 "b5057d15ebeca006cd335f63cfe232a20fe481dd5c3d199888ce51995c9f8b85"
     end
 
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
       url "https://cli.chkk.io/download/v#{version}/chkk-linux-arm64"
-      sha256 "66da7f52e3b8f4770900722ce4df74a777ce271658398da095648b82ced15833"
+      sha256 "a3cdd86bf50739218e52a9031361b909d6e67bc7532a5ba28c45d12714d2901a"
     end
   end
 
