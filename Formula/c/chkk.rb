94f354ec1081f86ec2715429675fe9bae311d016
Stefan Richter <44383+02strich@users.noreply.github.com>
2025-09-05 19:56:07 +0000

    chkk: update 0.0.7 bottle.

--- Formula/c/chkk.rb
+++ Formula/c/chkk.rb
@@ -5,9 +5,9 @@ class Chkk < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/chkk-io/cli"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "c21bf6cba9e5b06df220451af5a5bc6ceb22440087480dc80a61ab69f5703964"
-    sha256 cellar: :any_skip_relocation, ventura:       "d18834a139d2373561e703db032ea55028c48d822a7f44de4756b538fa3bd910"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "bd7f5ad5f8f612cdf663412816ccf083f5a70319ff73c1028717a50859a57e81"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "f56f7d12483cd3c4afd75521dde05f8ab2f094e66fc3c0cffce4b259112e27d5"
+    sha256 cellar: :any_skip_relocation, ventura:       "b1015947424e5c61948e898a9fd4eb8776601380de32574be61f945011cb3b95"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "853f1a3bbf5ab44297538e7eb0c82e01ae94d637fb80c61b9e92ca450f2ecd1d"
   end
 
   on_macos do
