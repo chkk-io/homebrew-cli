3c3261606ccbb22fbf4a8a4590b261f114ac16eb
Laiba Fatima Khan <laiba@chkk.io>
2026-05-12 19:49:45 +0500

    Update to v0.0.22
    Closes #20.
    
    Signed-off-by: Laiba Fatima Khan <108048669+laibafk@users.noreply.github.com>

--- Formula/c/chkk.rb
+++ Formula/c/chkk.rb
@@ -1,7 +1,7 @@
 class Chkk < Formula
   desc "Your Upgrade Copilot for k8s, Add-ons, Application services & 100s of Projects"
   homepage "https://chkk.io"
-  version "0.0.21"
+  version "0.0.22"
 
   bottle do
     root_url "https://ghcr.io/v2/chkk-io/cli"
@@ -12,24 +12,24 @@ class Chkk < Formula
   on_macos do
     if Hardware::CPU.arm?
       url "https://cli.chkk.io/download/v#{version}/chkk-darwin-arm64"
-      sha256 "dd597d157f65bf6168d2409ec26391b3f5133f3cdd39d766bfa4bbb28de03291"
+      sha256 "d38b72083e2b63dd893d923a2e542160618469af1aea9fca08449836f731e7ed"
     end
 
     if Hardware::CPU.intel?
       url "https://cli.chkk.io/download/v#{version}/chkk-darwin-amd64"
-      sha256 "5ee5f69c9c83073d6d32abb4a670808ada0e150aa47038a5de9833e3f183d8e6"
+      sha256 "1f5eaf3acd365d66d4015a52f2effb3504103b31c5b3ccc609c74d6b2b2612ba"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
       url "https://cli.chkk.io/download/v#{version}/chkk-linux-amd64"
-      sha256 "375109438570033b62bd3539805087c831b52c6ccbf29662d4f62d30528f8a4d"
+      sha256 "5bde0bc4631f37d6cb5cf99f7b052245c5a313db1917c4d5baa490779531625b"
     end
 
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
       url "https://cli.chkk.io/download/v#{version}/chkk-linux-arm64"
-      sha256 "f4ea3568d6a4a8d51c7ae6a42d294e551e6209c6f3844930bfac09c65731356c"
+      sha256 "a9bbea3bc2d7610248e0ee2db1eea0206aa18ca4c9fc7f4630263c389d658f48"
     end
   end
 
